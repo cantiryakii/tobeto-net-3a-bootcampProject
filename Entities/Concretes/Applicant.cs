29f0c11cdefe1b7aef44719f4193ce862edbf594
cantiryakii <mehmetcan__t@hotmail.com>
2024-02-16 12:32:06 +0300

    Add project files.

---
+++ Entities/Concretes/Applicant.cs
@@ -0,0 +1,27 @@
+﻿namespace Entities.Concretes
+{
+    //Başvuran
+    public class Applicant : User
+    {
+        public int UserId { get; set; }
+        public string About { get; set; }
+        public Applicant()
+        {
+        }
+
+        public Applicant(int id, int userId, string about, string userName, string firstName, string lastName,
+            DateTime dateOfBirth, string nationalIdentity, string eMail, string password)
+        {
+            Id = id;
+            UserId = userId;
+            About = about;
+            UserName = userName;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            NationalIdentity = nationalIdentity;
+            Email = eMail;
+            Password = password;
+        }
+    }
+}
---
+++ Entities/Concretes/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace Entities.Concretes
+{
+    public class Employee : User
+    {
+        public int UserId { get; set; }
+        public string Position { get; set; }
+
+        public Employee()
+        {           
+        }
+        public Employee(int id, int userId, string position, string userName, string firstName, string lastName,
+            DateTime dateOfBirth, string nationalIdentity, string eMail, string password)
+        {
+            Id = id;
+            UserId = userId;
+            Position = position;
+            UserName = userName;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            NationalIdentity = nationalIdentity;
+            Email = eMail;
+            Password = password;
+        }
+    }
+}
